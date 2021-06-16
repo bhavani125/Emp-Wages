50b5e8f19d1a11cf4f25c2e43ffa06f5dfb0b7b1
bhavani125 <girinenibhavani12@gmil.com>
2021-06-16 17:46:45 +0530

    added code to find employee pattimewage

--- EmpWages.java
+++ EmpWages.java
@@ -2,24 +2,31 @@ public class EmpWages
 {
 	//CONSTANTS
 	public static int isFullTime=1;
+	public static int isPartTime=2;
 	public static int WagePerHr=20;
   public static void main(String[] args)
   {
 	  // LOCAL VARIABLES
      int DailyWage;
      int WorkingHrs=0;
+	 int EmpWage=0;
 	 // COMPUTATION
-     double empcheck = Math.floor(Math.random() * 10) % 2;
+     double empcheck = Math.floor(Math.random() * 10) % 3;
      if(empcheck == isFullTime)
      {
        System.out.println("Employee is present");
        WorkingHrs=8;
      }
-     else
-      {
+     else if (empcheck == isPartTime)
+	 {
+		 System.out.println("Employee is present but for PartTime");
+          WorkingHrs=4;
+     }
+	 else
+     {
         System.out.println("Employee is Absent");
         WorkingHrs=0;
-      }
+     }
      DailyWage=WorkingHrs * WagePerHr;
      System.out.println("Daily wage of employee is " + DailyWage);
     }
