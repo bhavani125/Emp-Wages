7515013eebad3f52f47314621441e41f935a99b6
bhavani125 <girinenibhavani12@gmil.com>
2021-06-16 14:42:39 +0530

    Added code of Emp present or not

--- EmpWages.java
+++ EmpWages.java
@@ -2,6 +2,15 @@ public class EmpWages
 {
   public static void main(String[] args)
   {
-	  System.out.println("Welcome to Employee Wage Computation Program");
-  }
+	  // CONSTANTS
+     int isFullTime=1;
+	 // COMPUTATION
+     double empcheck = Math.floor(Math.random() * 10) % 2;
+     if(empcheck == isFullTime)
+     {
+       System.out.println("Employee is present");
+     }
+     else
+        System.out.println("Employee is Absent");
+	 }
 }
