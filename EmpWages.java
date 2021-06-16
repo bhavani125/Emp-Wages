3d3d5d0ae5e4c2f1448bc5f27778d201c715172c
bhavani125 <girinenibhavani12@gmil.com>
2021-06-16 17:32:46 +0530

    adding code to find daily emp wage

--- EmpWages.java
+++ EmpWages.java
@@ -1,16 +1,26 @@
 public class EmpWages
 {
+	//CONSTANTS
+	public static int isFullTime=1;
+	public static int WagePerHr=20;
   public static void main(String[] args)
   {
-	  // CONSTANTS
-     int isFullTime=1;
+	  // LOCAL VARIABLES
+     int DailyWage;
+     int WorkingHrs=0;
 	 // COMPUTATION
      double empcheck = Math.floor(Math.random() * 10) % 2;
      if(empcheck == isFullTime)
      {
        System.out.println("Employee is present");
+       WorkingHrs=8;
      }
      else
+      {
         System.out.println("Employee is Absent");
-	 }
+        WorkingHrs=0;
+      }
+     DailyWage=WorkingHrs * WagePerHr;
+     System.out.println("Daily wage of employee is " + DailyWage);
+    }
 }
