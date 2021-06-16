dffcb7a37ffc10abc5c9f9f032dd9b24e4b2e43e
bhavani125 <girinenibhavani12@gmil.com>
2021-06-16 18:13:57 +0530

    added code for calculating wages fora month of 20days

--- EmpWages.java
+++ EmpWages.java
@@ -4,16 +4,19 @@ public class EmpWages
 	public static int isFullTime=1;
 	public static int isPartTime=2;
 	public static int WagePerHr=20;
-  public static void main(String[] args)
+	public static int DaysInMonth=20;
+    public static void main(String[] args)
   {
 	  // LOCAL VARIABLES
      int DailyWage;
      int WorkingHrs=0;
 	 int EmpWage=0;
 	 // COMPUTATION
-     int empcheck = (int) Math.floor(Math.random() * 10) % 3;
-     switch (empcheck)
-     {
+	 for (int day=0; day<DaysInMonth; day++)
+     {    
+      int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+      switch (empcheck)
+      {
        case 1:
            System.out.println("Employee is present FullTime");
            WorkingHrs=8;
@@ -27,6 +30,9 @@ public class EmpWages
         WorkingHrs=0;
       }
      DailyWage=WorkingHrs * WagePerHr;
+	 totalDailyWage += DailyWage;
      System.out.println("Daily wage of employee is " + DailyWage);
     }
+	 System.out.println("Total employee wages for a month : " + toatlDailyWage)
+}
 }
